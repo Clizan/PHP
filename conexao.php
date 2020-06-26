34e48a9153412bde84cd3ea13590c5e709b8f07d
Clizan <31556355+Clizan@users.noreply.github.com>
2020-06-25 22:27:54 -0300

    Create conexao.php
    Página conexão

---
+++ conexao.php
@@ -0,0 +1,8 @@
+<?php 	
+	$servidor = "localhost"; /*maquina a qual o banco de dados está*/
+	$usuario = "root"; /*usuario do banco de dados MySql*/
+	$senha = ""; /*senha do banco de dados MySql*/
+	$banco = "crud"; /*seleciona o banco a ser usado*/
+
+	$conexao = mysqli_connect($servidor,$usuario,$senha, $banco);  /*Conecta no bando de dados MySql*/
+?>
